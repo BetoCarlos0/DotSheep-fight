d7e9413a686da80d84cc02ca67b618cc071328fa
Roberto Carlos <betocarlos00@hotmail.com>
2022-09-23 19:47:02 -0300

    add dockerfile e subido para heroku

--- DotSheepFight/obj/Debug/net6.0/DotSheepFight.AssemblyInfo.cs
+++ DotSheepFight/obj/Debug/net6.0/DotSheepFight.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("1cddffd0-8aa7-4f77-abe0-db34bec2a9ee")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotSheepFight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
